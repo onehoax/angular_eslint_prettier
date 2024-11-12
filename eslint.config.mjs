d18565c538eb79d8bf490bd1ab0cefbff7123c79
Andres Osorio <andres.osoriogr@gmail.com>
2024-11-11 19:37:50 -0500

    wt merge

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ export default [
           "error",
           {
             type: "element",
-            prefix: "myprefix",
+            prefix: "someprefix",
             style: "kebab-case",
           },
         ],
